a14015d64c2a39477979617c54e332f4b7d1cdd5
Prachi Kakkad <87642173+prachikakkad@users.noreply.github.com>
2022-10-16 23:35:20 +0530

    Update settings.py

--- Ecommerce_Website/settings.py
+++ Ecommerce_Website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-@4l&5kha4aik-p9upq2tor=%_2m+&dz%w5q9*lk1)o*3(25fwt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['jalaram-bandhani-house.herokuapp.com']
 
 
 # Application definition
