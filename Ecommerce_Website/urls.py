0b1d84a6bc93aa2a3039211de3d131fc23b8a20c
Prachi Kakkad <87642173+prachikakkad@users.noreply.github.com>
2022-10-17 15:51:46 +0530

    Update urls.py

--- Ecommerce_Website/urls.py
+++ Ecommerce_Website/urls.py
@@ -42,6 +42,7 @@ urlpatterns = [
     path('review', views.review, name="review"),
     path('paying-options', views.Paying_Options, name="paying-options"),
     path('paying-option-selected', views.Payment_Proceed, name="pay_proceed"),
+    path('reviews', views.Testimonials, name="Testimonials"),
     path('shop/', include('shop.urls'))
 ] 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
